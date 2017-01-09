49c5796889a3d0f7d993415196badf80d1472307
vijay.vepakomma@philips.com <vijay.vepakomma@philips.com>
2017-01-09 14:12:32 -0500

    Added Mvc and Logging

---
+++ src/dotnetstarter/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnethelloworld.Controllers
+{
+    public class MoviesController
+    {
+        public string Index()
+        {
+            return "<B>Movies</B>";
+        }
+    }
+}
--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 public class Startup
 {
-    public void Configure(IApplicationBuilder app)
+
+    public IConfiguration Configuration { get; set; }
+
+
+    public void ConfigureServices(IServiceCollection services)
     {
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+    {
+        loggerFactory.AddDebug();
+
         app.UseDefaultFiles();
         app.UseStaticFiles();
+
+        app.UseMvc(x => x.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
     }
+
 
     public static void Main(string[] args)
     {
